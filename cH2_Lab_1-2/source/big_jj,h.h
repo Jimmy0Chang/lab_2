e2e419fbc7aa261b3fa6f53dfc4687fdef76d24d
Jimmy0Chang <71509073+Jimmy0Chang@users.noreply.github.com>
2020-10-23 21:57:01 +0800

    Add files via upload

---
+++ cH2_Lab_1-2/source/big_jj,h.h
@@ -0,0 +1,7 @@
+int big_jj_1();
+int big_jj_2();
+int big_jj_3();
+int big_jj_4();
+int big_jj_5();
+
+
---
+++ cH2_Lab_1-2/source/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "big_jj.h"
+
+int main(void) {
+	big_jj_1();
+	big_jj_2();
+	big_jj_3();
+	big_jj_4();
+	big_jj_5();
+
+	printf("\n");
+	system("pause");
+}
